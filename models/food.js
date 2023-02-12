4b46d39783e328d138fc9af8850f9b5c371e5c9c
Jartius <JStavey13@users.noreply.github.com>
2023-02-12 13:46:31 -0500

    added food.js to the models folder. it is a template for a database of all food reviewed

---
+++ models/food.js
@@ -0,0 +1,39 @@
+const sequelize = require('../config/connections')
+const { Model, DataTypes }= require('sequelize')
+
+
+class Food extends Model {}
+
+
+Food.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+
+        food_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+
+        },
+
+        food_description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+
+        food_rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+
+    }
+)
+
+
+
+model.exports = Food
