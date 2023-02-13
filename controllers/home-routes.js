038c896105094bc5c78be46419c93b9f92e131a7
Jartius <JStavey13@users.noreply.github.com>
2023-02-13 14:15:03 -0500

    added route to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,6 +5,6 @@ router.get('/', async (req, res) => {
     const foodData = await Food.findAll().catch((err) => { 
         res.json(err);
       });
-        const dishes = dishData.map((dish) => dish.get({ plain: true }));
-        res.render('all', { dishes });
+        const foods = foodData.map((dish) => dish.get({ plain: true }));
+        res.render('homepage', { foods });
       });
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 
