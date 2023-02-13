95b1f3d7ad3eb0c622371a844661aca2b10ad2ed
Jartius <JStavey13@users.noreply.github.com>
2023-02-13 14:39:01 -0500

    added a login route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,8 +9,15 @@ router.get('/', async (req, res) => {
         res.render('homepage', { foods });
       });
 
-
-
+      router.get('/login', (req, res) => {
+      
+        if (req.session.logged_in) {
+          res.redirect('/');
+          return;
+        }
+      
+        res.render('login');
+      });
 
 
 module.exports = router
