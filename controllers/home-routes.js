09e3b5abdd7eed5b047b99a361728f73b0b0bdb2
Jartius <JStavey13@users.noreply.github.com>
2023-02-14 14:39:50 -0500

    got maps on the site with markers. added login functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -19,5 +19,22 @@ router.get('/', async (req, res) => {
         res.render('login');
       });
 
+      router.get('/food', async (req, res) => {
+        const foodData = await Food.findAll().catch((err) => { 
+            res.json(err);
+          });
+            const foods = foodData.map((food) => food.get({ plain: true }));
+            res.render('food', { foods });
+          });
+    
+          router.get('/login', (req, res) => {
+          
+            if (req.session.logged_in) {
+              res.redirect('/');
+              return;
+            }
+          
+            res.render('login');
+          });
 
 module.exports = router
